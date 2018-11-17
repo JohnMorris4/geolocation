e238ca2558bb0a91468b42e5a52a0b44bc2aa1a3
John Morris <johnmorris4@bellsouth.net>
2018-11-17 04:34:23 -0500

    Building different methods

--- src/index.js
+++ src/index.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-import SeasonDisplay from './SeasonDisplay';
+// import SeasonDisplay from './SeasonDisplay';
  
 class App extends Component {
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    ); 
-    return (
+  constructor(props) {
+    // Onetime setup
+    super(props);
 
-      <div>Latitude:   </div>
-    );
+    this.state = { lat: null, errorMessage: '' };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      // ALLWAYS HAVE A CALLBACK TO THE ERR CALLBACK
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    ); 
+  }
+  componentDidMount = () => {
+    console.log('My component was rendered to the screen');
+  }
+
+  componentDidUpdate = () => {
+    console.log('My component did update and rerendered');
+  }
+  
+  
+
+  render() {
+    // return JSX
+    if(this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+    if(!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return <div>Loading...</div>
+    
   }
 
 
